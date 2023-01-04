51a29ff4a0128391929c28c6a2d15884331465c2
Vitalii MV <agronom9913@gmail.com>
2023-01-04 22:24:12 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function hoverOnLeft() {
+    document.getElementById("div__right").classList.add("active");
+}
+
+function hoverOffLeft() {
+    document.getElementById("div__right").classList.remove("active");
+ }
+
+function hoverOnRight() {
+    document.getElementById("div__left").classList.add("active");
+
+}
+
+function hoverOffRight() {
+    document.getElementById("div__left").classList.remove("active");
+
+}
